ab5e270da45cc54a650273af1c3d9f9ae5ecd56d
unknown <mejbacsedu@gmail.com>
2022-10-12 08:02:43 +0600

    charts added

--- src/Pages/Statistics/Statistics.js
+++ src/Pages/Statistics/Statistics.js
@@ -4,39 +4,31 @@ import { Pie, PieChart, Tooltip } from 'recharts';
 
 export default function Statistics() {
     
-      const data02 = [
-        {
-          "name": "Group A",
-          "value": 2400
-        },
-        {
-          "name": "Group B",
-          "value": 4567
-        },
-        {
-          "name": "Group C",
-          "value": 1398
-        },
-        {
-          "name": "Group D",
-          "value": 9800
-        },
-        {
-          "name": "Group E",
-          "value": 3908
-        },
-        {
-          "name": "Group F",
-          "value": 4800
-        }
-      ];
+      
     const quizItem = useLoaderData();
   return (
-    <div>
+    <div className='container'>
+    <h1 className='text-uppercase text-md-center fw-lighter my-3 border border-3 border-primary d-inline-block p-2'>Quiz Total Statistics</h1>
+    <hr  className=''/>
+    <div className='row mt-4 p-2 g-md-2'>
+        <div className='col-md-6 col-12'>
         <PieChart width={730} height={250}>
         <Pie data={quizItem.data} dataKey='total' nameKey="name" cx="50%" cy="50%" innerRadius={60} outerRadius={80} fill="#82ca9d" label />
         <Tooltip />
         </PieChart>
+        </div>
+        <div className='col-md-6 col-12 d-flex justify-content-start align-items-center'>
+          <ul className='text-decoration-none'>
+          {
+            quizItem.data.map(quiz=><li 
+            key={quiz.id}
+            >
+              {quiz.name} : {quiz.total}
+            </li>)
+          }
+          </ul>
+        </div>
+    </div>
     </div>
   )
 }
